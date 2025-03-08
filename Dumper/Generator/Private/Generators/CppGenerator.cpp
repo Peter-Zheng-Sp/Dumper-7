64034cdb51fba56ee3ed291ec90833403df2a477
Fischsalat <64608145+Fischsalat@users.noreply.github.com>
2025-03-08 13:46:41 +0100

    Update CppGenerator.cpp

--- Dumper/Generator/Private/Generators/CppGenerator.cpp
+++ Dumper/Generator/Private/Generators/CppGenerator.cpp
@@ -3278,7 +3278,7 @@ class UClass* StaticBPGeneratedClassImpl()
 template<class ClassType>
 ClassType* GetDefaultObjImpl()
 {
-	return static_cast<ClassType*>(ClassType::StaticClass()->DefaultObject);
+	return reinterpret_cast<ClassType*>(ClassType::StaticClass()->DefaultObject);
 }
 
 )";
